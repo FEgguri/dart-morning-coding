addf4fa951a38579c4f942d970224fc90329bd62
leeys <95343772+agent-megurii@users.noreply.github.com>
2025-07-08 09:33:59 +0900

    아침의코딩 7월 8일

---
+++ lib/code_0708.dart
@@ -0,0 +1,15 @@
+class Solution {
+  int tribonacci(int n) {
+    List<int> triList = [0, 1, 1];
+    for (int i = 3; i <= n; i++) {
+      triList.add(triList[i - 1] + triList[i - 2] + triList[i - 3]);
+    }
+    print(triList);
+    return triList[n];
+  }
+}
+
+void main() {
+  var soultion = Solution();
+  print(soultion.tribonacci(6));
+}
