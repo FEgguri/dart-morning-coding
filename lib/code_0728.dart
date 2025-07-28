2a4279b203fb4d2503b98661ef2a6a15ba94b611
leeys <95343772+agent-megurii@users.noreply.github.com>
2025-07-28 09:39:12 +0900

    933. 최근 호출 수

---
+++ lib/code_0728.dart
@@ -0,0 +1,23 @@
+import 'dart:collection';
+
+class RecentCounter {
+  Queue<int> q = Queue<int>();
+  RecentCounter();
+
+  int ping(int t) {
+    q.addLast(t);
+
+    if (q.isNotEmpty && q.first < t - 3000) {
+      q.removeFirst();
+    }
+
+    return q.length;
+  }
+}
+
+void main() {
+  RecentCounter recentCounter = RecentCounter();
+  recentCounter.ping(1);
+  recentCounter.ping(100);
+}
+//Runtime: 2 ms
