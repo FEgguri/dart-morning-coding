ef78acd427a9ef5c301b1a595f0e62f460e140ff
guri <youguli96@gmail.com>
2025-07-31 09:37:24 +0900

    m

--- lib/code_0731.dart
+++ lib/code_0731.dart
@@ -5,25 +5,18 @@ void main() {
 }
 
 class Solution {
-  //bool process = false;
   int get = 0;
   int sumProfit = 0;
 
   int maxProfit(List<int> prices) {
-    int maxValue = prices.reduce((a, b) => a > b ? a : b);
     get = prices[0];
     for (int i = 0; i <= prices.length - 1; i++) {
-      //get > lenght = buy
       if (get > prices[i]) {
-        print("buyprocess \n get : $get \n price : ${prices[i]}");
         get = prices[i];
       } else if (get < prices[i]) {
-        print("sellprocess \n get : $get \n price : ${prices[i]}");
         sumProfit += prices[i] - get;
         get = prices[i];
       }
-      //get < lenght = sell => sumprofit
-      //result
     }
     return sumProfit;
   }
