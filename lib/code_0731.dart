387914de0217db4c08b92dc30173a3f5e03eb334
guri <youguli96@gmail.com>
2025-07-31 09:47:50 +0900

    122

--- lib/code_0731.dart
+++ lib/code_0731.dart
@@ -7,11 +7,15 @@ void main() {
 class Solution {
   int get = 0;
   int sumProfit = 0;
+  bool buyprocess = false;
 
   int maxProfit(List<int> prices) {
     get = prices[0];
     for (int i = 0; i <= prices.length - 1; i++) {
-      if (get > prices[i]) {
+      if (i != 0 && prices[i - 1] > prices[i]) {
+        // if (get > prices[i]) {
+        //   get = prices[i];
+        // }
         get = prices[i];
       } else if (get < prices[i]) {
         sumProfit += prices[i] - get;
